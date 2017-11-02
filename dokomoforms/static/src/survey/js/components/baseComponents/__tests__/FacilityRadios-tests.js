ca2ccb1bbf126931b4f9c8cd04ff190f84ed3b2a
jmbott <millerbott@gmail.com>
2017-11-02 13:24:39 -0400

    react-dom/test-utils swap

--- dokomoforms/static/src/survey/js/components/baseComponents/__tests__/FacilityRadios-tests.js
+++ dokomoforms/static/src/survey/js/components/baseComponents/__tests__/FacilityRadios-tests.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
+import TestUtils from 'react-dom/test-utils';
 
 // a noop function useful for passing into components that require it.
 var noop = () => {},
