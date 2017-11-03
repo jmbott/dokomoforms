975dc4775b2becd226c1d235d655bc5c239522ff
jmbott <millerbott@gmail.com>
2017-11-03 12:24:59 -0400

    firefox 54

--- dokomoforms/static/src/survey/js/components/baseComponents/__tests__/DontKnow-tests.js
+++ dokomoforms/static/src/survey/js/components/baseComponents/__tests__/DontKnow-tests.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-dom/test-utils';
-import PropTypes from 'prop-types';
 
 // a noop function useful for passing into components that require it.
 var noop = () => {};
