84b10f2d402d203cb2814e2cb93684ccb83a0ae4
jmbott <millerbott@gmail.com>
2018-01-31 15:34:30 -0500

    polyfill

--- dokomoforms/static/src/survey/js/components/__tests__/Facility-tests.js
+++ dokomoforms/static/src/survey/js/components/__tests__/Facility-tests.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-dom/test-utils';
+import 'raf/polyfill';
 
 // jest.autoMockOff();
 
--- dokomoforms/static/src/survey/js/components/baseComponents/__tests__/FacilityRadios-tests.js
+++ dokomoforms/static/src/survey/js/components/baseComponents/__tests__/FacilityRadios-tests.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-dom/test-utils';
+import 'raf/polyfill';
 
 // a noop function useful for passing into components that require it.
 var noop = () => {},
